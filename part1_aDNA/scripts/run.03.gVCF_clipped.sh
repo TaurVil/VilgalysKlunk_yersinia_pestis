39e13365753ca50193d388489610e41996924d1c
TaurVil <41305538+TaurVil@users.noreply.github.com>
2022-11-04 10:56:48 -0500

    Update run.03.gVCF_clipped.sh

--- part1_aDNA/scripts/run.03.gVCF_clipped.sh
+++ part1_aDNA/scripts/run.03.gVCF_clipped.sh
@@ -5,9 +5,9 @@ module load java; module load samtools
 g=${SLURM_ARRAY_TASK_ID}
 f=`head -$g 00_mapDamage_bams.list | tail -1`;
 
-samtools addreplacerg -r ID:$f -r PL:Illumina -r SM:$f -r LB:$f final_bams/$f.bam -o final_bams/rg.$f.bam
-samtools index final_bams/rg.$f.bam
+samtools addreplacerg -r ID:$f -r PL:Illumina -r SM:$f -r LB:$f bams_trim4/$f.bam -o bams_trim4/rg.$f.bam
+samtools index bams_trim4/rg.$f.bam
 
-Programs/gatk-4.1.4.1/gatk HaplotypeCaller -ERC GVCF -I final_bams/rg.$f.bam  -R my_genomes/hg19/hg19.fa  -O gVCF/$f.g.vcf.gz -mbq 20 --sample-name $f
+Programs/gatk-4.1.4.1/gatk HaplotypeCaller -ERC GVCF -I bams_trim4/rg.$f.bam  -R my_genomes/hg19/hg19.fa  -O gVCF/$f.g.vcf.gz -mbq 20 --sample-name $f
 
 echo $f $g
